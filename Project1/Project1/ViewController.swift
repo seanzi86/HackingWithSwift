4f31a5d03f000a95c1ac1744b79ca3496742cefb
Sean Carrington <seanzi@msn.com>
2019-02-21 23:12:38 +0000

    Added project 1.

---
+++ Project1/Project1/ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Sean Carrington on 21/02/2019.
+//  Copyright © 2019 seanzi86. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let fileManager = FileManager.default;
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path);
+        
+        for item in items{
+            if (item.hasPrefix("nssl")){
+                pictures.append(item);
+            }
+        }
+        
+        print(pictures)
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //1: try loading the "Detail" view controller and typecasting it to be DetailViewController.
+        if let viewController = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            //2. success! Set its selectedImage property.
+            viewController.selectedImage = pictures[indexPath.row];
+            
+            //3. now push it onto the navigation controller.
+            navigationController?.pushViewController(viewController, animated: true)
+        }
+    }
+}
+
