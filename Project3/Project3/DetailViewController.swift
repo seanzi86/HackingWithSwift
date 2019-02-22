42e4aef09b9f5233a2902648d9cb6898094bfafd
Sean Carrington <seanzi@msn.com>
2019-02-22 17:39:15 +0000

    Added Project3

---
+++ Project3/Project3/DetailViewController.swift
@@ -0,0 +1,46 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Sean Carrington on 21/02/2019.
+//  Copyright © 2019 seanzi86. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
+    var selectedImageNumber: Int?
+    var imageCount: Int?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Picture \(selectedImageNumber!) of \(imageCount!)"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated);
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+    
+    @objc func shareTapped(){
+        let viewController = UIActivityViewController(activityItems: [imageView.image!], applicationActivities: [])
+        viewController.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(viewController, animated: true)
+        
+    }
+}
