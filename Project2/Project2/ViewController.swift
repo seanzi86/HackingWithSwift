8b42ea52a32619a24f248fa65a440536042742aa
Sean Carrington <seanzi@msn.com>
2019-02-22 16:08:40 +0000

    Added Project2

---
+++ Project2/Project2/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Sean Carrington on 22/02/2019.
+//  Copyright © 2019 seanzi86. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var countries = [String]()
+    var correctAnswer = 0
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries.append("estonia")
+        countries.append("france")
+        countries.append("germany")
+        countries.append("ireland")
+        countries.append("italy")
+        countries.append("monaco")
+        countries.append("nigeria")
+        countries.append("poland")
+        countries.append("russia")
+        countries.append("spain")
+        countries.append("uk")
+        countries.append("us")
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil){
+        countries.shuffle()
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        correctAnswer = Int.random(in:0...2)
+        title = countries[correctAnswer].uppercased()
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if (sender.tag == correctAnswer){
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Incorrect"
+            score -= 1
+        }
+        
+        let alertController = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        present(alertController, animated: true)
+    }
+    
+}
+
