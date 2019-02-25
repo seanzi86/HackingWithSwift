412739a6af0b539624ea9fd20c2de756439ebdb0
Sean Carrington <seanzi@msn.com>
2019-02-25 21:42:00 +0000

    Added project 7

---
+++ Project7/Project7/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Project7
+//
+//  Created by Sean Carrington on 25/02/2019.
+//  Copyright © 2019 seanzi86. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    var petitions = [Petition]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+        } else {
+            urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+        }
+        
+        if let url = URL(string: urlString) {
+            if let data = try? Data(contentsOf: url) {
+                parse(json: data)
+                return
+            }
+        }
+        
+        showError()
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let viewController = DetailViewController()
+        viewController.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(viewController, animated: true)
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let petition = petitions[indexPath.row]
+        cell.textLabel?.text = petition.title
+        cell.detailTextLabel?.text = petition.body
+        return cell
+    }
+    
+    func parse(json: Data) {
+        let decoder = JSONDecoder()
+        
+        if let jsonPetitions = try? decoder.decode(Petitions.self, from: json){
+            petitions = jsonPetitions.results
+            tableView.reloadData()
+        }
+    }
+    
+    func showError() {
+        let alertController = UIAlertController(title: "Loading error", message:"There was a problem loading the feed; please check your connection and try again", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title:"OK", style:.default))
+        present(alertController, animated: true)
+    }
+
+
+}
+
