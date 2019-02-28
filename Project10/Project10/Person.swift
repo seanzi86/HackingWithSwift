b85d9f500ff4602b3eeb793ba758ef0347cf957d
Sean Carrington <seanzi@msn.com>
2019-02-28 23:01:40 +0000

    Added Project 10

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Sean Carrington on 28/02/2019.
+//  Copyright © 2019 seanzi86. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+}
