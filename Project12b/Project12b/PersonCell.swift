204b0a8e81a4c8bf18eae2f629ce79bb71253bee
Sean Carrington <seanzi@msn.com>
2019-03-04 21:42:44 +0000

    Added projects 12a and 12b

---
+++ Project12b/Project12b/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Sean Carrington on 28/02/2019.
+//  Copyright © 2019 seanzi86. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
