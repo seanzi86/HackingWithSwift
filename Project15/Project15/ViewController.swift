bb7c9e6c895b6802673c32e9e0e42e9d60feb220
Sean Carrington <seanzi@msn.com>
2019-03-08 22:22:59 +0000

    Added Project 15

---
+++ Project15/Project15/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Project15
+//
+//  Created by Sean Carrington on 08/03/2019.
+//  Copyright © 2019 seanzi86. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var tap: UIButton!
+    var imageView: UIImageView!
+    var currentAnimation = 0
+    
+    
+    @IBAction func tapped(_ sender: Any) {
+        
+        tap.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay:0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [],
+                       animations: { [unowned self] in
+                        switch self.currentAnimation {
+                        case 1,3,5:
+                            self.imageView.transform = CGAffineTransform.identity
+                        case 0:
+                            self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
+                        case 2:
+                            self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+                        case 4:
+                            self.imageView.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+                        case 6:
+                            self.imageView.alpha = 0.1
+                            self.imageView.backgroundColor = UIColor.green
+                        case 7:
+                            self.imageView.alpha = 1
+                            self.imageView.backgroundColor = UIColor.clear
+                            
+                        default:
+                            break
+                        }
+        }) { [unowned self] (finished: Bool) in
+            self.tap.isHidden = false
+        }
+        
+        currentAnimation += 1
+        if currentAnimation > 7 {
+            currentAnimation = 0
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView = UIImageView(image: UIImage(named: "penguin"))
+        imageView.center = CGPoint(x: 512, y: 384)
+        view.addSubview(imageView)
+
+    }
+
+
+}
+
