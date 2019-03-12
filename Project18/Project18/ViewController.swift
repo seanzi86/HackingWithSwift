c312fcb465d00d2c40bbfd0e47b29bc3ec601ee1
Sean Carrington <seanzi@msn.com>
2019-03-12 20:02:59 +0000

    Added project 18

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Sean Carrington on 12/03/2019.
+//  Copyright © 2019 seanzi86. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
