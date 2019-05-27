671c5a12b1c58efdef64e4c0ab9c06521d9435da
Sean Carrington <seanzi@msn.com>
2019-05-27 17:38:11 +0100

    Added Project 34

---
+++ Project34/Project34/Player.swift
@@ -0,0 +1,42 @@
+//
+//  Player.swift
+//  Project34
+//
+//  Created by Sean Carrington on 27/05/2019.
+//  Copyright © 2019 seanzi86. All rights reserved.
+//
+
+import UIKit
+import GameplayKit
+
+class Player: NSObject, GKGameModelPlayer {
+    var chip: ChipColor
+    var color: UIColor
+    var name: String
+    var playerId: Int
+    
+    static var allPlayers = [Player(chip: .red), Player(chip: .black)]
+    
+    init(chip: ChipColor) {
+        self.chip = chip
+        self.playerId = chip.rawValue
+        
+        if chip == .red {
+            color = .red
+            name = "Red"
+        } else {
+            color = .black
+            name = "Black"
+        }
+        
+        super.init()
+    }
+    
+    var opponent: Player {
+        if chip == .red {
+            return Player.allPlayers[1]
+        } else {
+            return Player.allPlayers[0]
+        }
+    }
+}
